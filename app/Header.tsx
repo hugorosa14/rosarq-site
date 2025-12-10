8405bb2e3d1860ade9c026ed6cabf8b4ba0d4554
hugorosa14 <hugo.rosa.14@hotmail.com>
2025-12-10 13:52:48 +0100

    Fix mobile menu style and background opacity

--- app/Header.tsx
+++ app/Header.tsx
@@ -63,10 +63,10 @@ export default function Header() {
 
       {/* MOBILE SLIDE MENU */}
       <div
-  className={`md:hidden fixed top-0 left-0 w-3/4 h-full 
-  bg-[#f9f4ef] shadow-xl z-40 
-  transform transition-transform duration-300
-  ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
+  className={`md:hidden fixed top-0 left-0 w-3/4 h-full overflow-y-auto
+    bg-[#f9f4ef]/80 backdrop-blur-lg shadow-xl z-40 p-6
+    ${isOpen ? "translate-x-0" : "-translate-x-full"}
+  `}
 >
   <nav className="flex flex-col gap-6 text-lg font-medium text-slate-900 mt-10 p-6">
     <a href="#services" onClick={closeMenu}>Services</a>
