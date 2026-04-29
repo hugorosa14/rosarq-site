d2f798241f5a395eb707a0f2b0e2c15b2bbf703c
hugorosa14 <hugo.rosa.14@hotmail.com>
2026-04-29 12:48:55 +0200

    Add sitemap and robots.txt for SEO and Google indexing

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://rosartifex.lu',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1.0,
+    },
+  ];
+}
